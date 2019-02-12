1c4dbf91cb1f26830afbe68c4bd99df76214de38
avicndugu <avicnduguh@gmail.com>
2019-02-12 12:03:26 +0300

    Multiple consecutive spaces now visible

--- 01-KeysPressed/main2.js
+++ 01-KeysPressed/main2.js
@@ -7,10 +7,15 @@ let allToType=[];
 // let toTypeText=[];
 // let t;
 
+let textView2=document.querySelector('#text-view2');
+let textView3=document.querySelector('#text-view3');
+
 // This displays the text that should be typed. The n value should be used.
-console.log(generatedText.length);
+// console.log(generatedText.length);
 toType=generatedText.slice(n,generatedText.length);
-console.log(toType);
+// console.log(toType);
+textView3.innerText=toType.slice(n);
+
 
 
 
@@ -22,12 +27,17 @@ console.log(toType);
 // Pressed button function
 function userPress(e){
 	// Adds a character that have been typed
+	console.log(e.key);
 	if (e.key.length<2){
+		// if(e.key===' '){
+			// e.key="*";
+			// console.log(e.key);
+		// }
 		typedText.push(e.key);
 		// When typing the the n value should increase
 		n=n+1;
 		// console.log(n);
-		console.log(typedText);
+		// console.log(typedText);
 	}
 	else{
 		// Delete character from the typed characters
@@ -37,10 +47,11 @@ function userPress(e){
 			if (n>0){
 				n=n-1;
 			}
-			console.log(typedText);
+			// console.log(typedText);
 		}
 	}
-	console.log(n);
+	// console.log(typedText);
+	// console.log(n);
 	typing();
 	dummy();
 	return typedText;
@@ -49,10 +60,15 @@ function userPress(e){
 // Use this for displaying the text typed to view
 function typing(){
 	typed=typedText.join("");
+	// Allows for multiple space to be displayed.
+	// InnerHTML and innerText shows only the first spacing and ignores the rest
+	let viewType = typed.replace(/ /g, "\u00A0");
 	// Display text that is being deleted
-	console.log(toType.slice(n,generatedText.length));
-	console.log(typed);
-	return typed;
+	// console.log(toType.slice(n,generatedText.length));
+	// console.log(viewType);
+	// console.log(typed.length);
+	textView2.innerText=viewType;
+	// return viewType;
 }
 
 
@@ -60,13 +76,17 @@ function typing(){
 // Check for correctness of the typed text and the text to be typed
 
 function dummy(){
-	console.log(toType.slice(0,n));
-	console.log(typed);
+	// console.log(toType.slice(0,n));
+	textView3.innerText=toType.slice(n);
+
+	// console.log(typed);
 	if (toType.slice(0,n)===typed){
-		console.log(1);
+		// console.log(1);
+		textView2.style.color='green';
 	}
 	else{
-		console.log(3);
+		// console.log(3);
+		textView2.style.color='red';
 	}
 // // allToType.push(to)
 }
